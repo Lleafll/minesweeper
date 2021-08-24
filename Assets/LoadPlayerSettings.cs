52d34cbbc76a3871933bf9e9c57cf3c6163eb979
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-08-24 17:42:35 +0200

    Load player settings on StartScene start up and fill fields accordingly

---
+++ Assets/LoadPlayerSettings.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadPlayerSettings : MonoBehaviour
+{
+    [SerializeField] private PlayerSettings settings;
+    [SerializeField] private InputField mineCount;
+    [SerializeField] private InputField rows;
+    [SerializeField] private InputField columns;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mineCount.text = settings.GetMineCount().ToString();
+        rows.text = settings.GetRowCount().ToString();
+        columns.text = settings.GetColumnCount().ToString();
+    }
+}
