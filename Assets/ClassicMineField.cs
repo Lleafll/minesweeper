0a581c990eeab2b7323d6f71b2ddb47622467ef3
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-08-26 20:39:07 +0200

    Implement ClassicMineField.GenerateTiles() in terms of StaticTileGenerator

--- Assets/ClassicMineField.cs
+++ Assets/ClassicMineField.cs
@@ -68,51 +68,8 @@ public class ClassicMineField
 
     private void GenerateTiles(bool[,] mines)
     {
-        tiles = new Tile[rows, columns];
-        for (int row = 0; row < rows; row++)
-        {
-            for (int column = 0; column < columns; column++)
-            {
-                if (mines[row, column])
-                {
-                    tiles[row, column] = Tile.Mine;
-                }
-                else
-                {
-                    tiles[row, column] = Tile.Empty;
-                }
-            }
-        }
-        for (int row = 0; row < rows; row++)
-        {
-            for (int column = 0; column < columns; column++)
-            {
-                if (tiles[row, column] == Tile.Mine)
-                {
-                    continue;
-                }
-                var minesInProximity = 0;
-                for (int x = row - 1; x <= row + 1; x++)
-                {
-                    if (x < 0 || x >= rows)
-                    {
-                        continue;
-                    }
-                    for (int y = column - 1; y <= column + 1; y++)
-                    {
-                        if (y < 0 || y >= columns)
-                        {
-                            continue;
-                        }
-                        if (tiles[x, y] == Tile.Mine)
-                        {
-                            minesInProximity++;
-                        }
-                    }
-                }
-                tiles[row, column] = (Tile)minesInProximity;
-            }
-        }
+        var generator = new StaticTileGenerator(mines);
+        tiles = generator.Generate();
     }
 
     private void GenerateFog()
